2434f8980be4ae9e038888968555a553a4aabd51
Comodoro180 <carlos.arturo.hoyos@gmail.com>
2018-03-09 23:01:56 -0500

    Usuario con clave encriptada

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -4,63 +4,87 @@ class ControladorUsuarios {
     /*
      * INGRESO DE USUARIOS
      */
+
     static public function ctrIngresoUsuario() {
-        
-        if(isset($_POST["ingEmail"]) && isset($_POST["ingClave"])){
-            
+
+        if (isset($_POST["ingEmail"]) && isset($_POST["ingClave"])) {
+
             $tabla = "tusuario";
-            
             $campo = "email";
             $valor = $_POST["ingEmail"];
             
+            $clave = crypt($_POST["ingClave"],'$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
             $respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $campo, $valor);
-           
-            //print $respuesta["EMAIL"];
-            
-            if($respuesta["EMAIL"] == $_POST["ingEmail"] && $respuesta["CLAVE"] == $_POST["ingClave"]){
-                
+
+            if ($respuesta["EMAIL"] == $_POST["ingEmail"] && $respuesta["CLAVE"] == $clave) {
+
                 $_SESSION["iniciarSesion"] = "ok";
-                
-                echo '<script>
-                    
-                    window.location = "inicio";
-                    
+
+                echo '<script>                    
+                    window.location = "inicio";                    
                  </script>';
-                
-            } else{
-                
+            } else {
+
                 echo '<div class="alert alert-danger"> Error al ingresar, vuelve a intentarlo</div>';
-                
             }
-            
         }
     }
 
     /*
      * CREAR USUARIO
      */
+
     static public function ctrCrearUsuario() {
 
         if (isset($_POST["nuevoEmail"])) {
             if (preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoNombre"]) &&
                 preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoApellido"])) {
+
+                $tabla = "tusuario";
                 
+                $encriptar = crypt($_POST["nuevaClave"],'$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+                
+                $datos = array(
+                    "nombre"   => $_POST["nuevoNombre"],
+                    "apellido" => $_POST["nuevoApellido"],
+                    "email"    => $_POST["nuevoEmail"],
+                    "clave"    => $encriptar,
+                    "idperfil" => $_POST["nuevoPerfil"]
+                );
+                
+                $respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla,$datos);
+                
+                if ($respuesta == "ok"){
                     echo '<script>
-                        swal({
-                                type: "success",
-                                title: "¡El usuario ha sido guardado correctamente!",
-                                showConfirmButton: true,
-                                confirmButtonText: "Cerrar"
+                            swal({
+                                    type: "success",
+                                    title: "¡El usuario ha sido guardado correctamente!",
+                                    showConfirmButton: true,
+                                    confirmButtonText: "Cerrar"
 
-                        }).then(function(result){
-                                if(result.value){
-                                        window.location = "usuarios";
-                                }
-                        });
-                    </script>';
-                
+                            }).then(function(result){
+                                    if(result.value){
+                                            window.location = "usuarios";
+                                    }
+                            });
+                        </script>';
+                } else {
+                        echo '<script>
+                                swal({
+                                        type: "error",
+                                        title: "'.$respuesta.'",
+                                        showConfirmButton: true,
+                                        confirmButtonText: "Cerrar"
+
+                                }).then(function(result){
+                                        if(result.value){
+                                                window.location = "usuarios";
+                                        }
+                                });
+                        </script>';                    
+                }                
             } else {
-
                 echo '<script>
                         swal({
                                 type: "error",
@@ -78,16 +102,17 @@ class ControladorUsuarios {
         }
     }
 
-	/*=============================================
-	MOSTRAR USUARIO
-	=============================================*/
+    /* =============================================
+      MOSTRAR USUARIO
+      ============================================= */
 
-	static public function ctrMostrarUsuarios($campo, $valor){
+    static public function ctrMostrarUsuarios($campo, $valor) {
 
-		$tabla = "tusuario";
+        $tabla = "tusuario";
 
-		$respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $campo, $valor);
+        $respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $campo, $valor);
 
-		return $respuesta;
-	}    
+        return $respuesta;
+    }
+
 }
--- modelos/usuarios.modelo.php
+++ modelos/usuarios.modelo.php
@@ -3,39 +3,53 @@
 require_once "conexion.php";
 
 class ModeloUsuarios {
+    
     /* =============================================
       MOSTRAR USUARIOS
       ============================================= */
-
     static public function mdlMostrarUsuarios($tabla, $campo, $valor) {
         try {
 		if($campo != null){
 
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $campo = :$campo");
-
 			$stmt -> bindParam(":".$campo, $valor, PDO::PARAM_STR);
-
 			$stmt -> execute();
-
 			return $stmt -> fetch();
-
 		}else{
 
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
-
 			$stmt -> execute();
-
 			return $stmt -> fetchAll();
-
-		}
-		
+		}		
 
 		$stmt -> close();
-
 		$stmt = null;
+                
         } catch (PDOException $ex) {
             print "ERROR" . $ex->getMessage();
+            return "ERROR" . $ex->getMessage();
         }
     }
+    
+    static public function mdlIngresarUsuario($tabla,$datos){
+        try {
+            
+            $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(nombre, apellido, email, clave, estado, idperfil) "
+                    . "VALUES (:nombre, :apellido, :email, :clave, 0, :idperfil)");
 
+            $stmt->bindParam(":nombre", $datos["nombre"],PDO::PARAM_STR);
+            $stmt->bindParam(":apellido", $datos["apellido"],PDO::PARAM_STR);
+            $stmt->bindParam(":email", $datos["email"],PDO::PARAM_STR);
+            $stmt->bindParam(":clave", $datos["clave"],PDO::PARAM_STR);
+            $stmt->bindParam(":idperfil", $datos["idperfil"],PDO::PARAM_STR);
+            
+            if ($stmt->execute()){
+                return "ok";
+            }
+            
+        } catch (PDOException $ex) {
+            print "ERROR" . $ex->getMessage();
+            return $ex->getMessage();
+        } 
+    }
 }
