be705f57d998057e4faaec6e50583342e7c81fb1
Juan Sebastian Soto Castro <44708632+Soto1311@users.noreply.github.com>
2018-11-08 15:00:17 -0500

    CRUD Proveedores

---
+++ controladores/proveedores.controlador.php
@@ -0,0 +1,151 @@
+<?php
+
+class ControladorProveedores{
+
+    /*=============================================
+      CREAR PROVEEDOR
+    =============================================*/
+    static public function ctrCrearProveedor() {
+
+        if (isset($_POST["nuevoProveedor"])) {
+            
+                $datos = array( "nit"         => $_POST["nuevoNit"],
+                                "nombre"      => $_POST["nuevoProveedor"],
+                                "direccion"   => $_POST["nuevaDireccion"],
+                                "telefono1"   => $_POST["nuevoTelefono1"],
+                                "telefono2"   => $_POST["nuevoTelefono2"],
+                                "idCiudad"    => $_POST["nuevaCiudad"]
+                );
+                
+                $respuesta = ModeloProveedores::mdlIngresarProveedor($datos);
+                
+                if ($respuesta == "ok"){
+                    echo '<script>
+                            swal({
+                                    type: "success",
+                                    title: "¡El proveedor ha sido guardado correctamente!",
+                                    showConfirmButton: true,
+                                    confirmButtonText: "Cerrar"
+
+                            }).then(function(result){
+                                    if(result.value){
+                                            window.location = "proveedores";
+                                    }
+                            });
+                        </script>';
+                } else {
+                        echo '<script>
+                                swal({
+                                        type: "error",
+                                        title: "'.$respuesta.'",
+                                        showConfirmButton: true,
+                                        confirmButtonText: "Cerrar"
+
+                                }).then(function(result){
+                                        if(result.value){
+                                                window.location = "proveedores";
+                                        }
+                                });
+                        </script>';                    
+                }                
+
+        }
+    }
+    
+
+    /*=============================================
+      MOSTRAR
+    =============================================*/
+    static public function ctrMostrarProveedores($campo, $valor) {
+        
+        include_once "../conf/config.inc.php";
+        
+        $respuesta = ModeloProveedores::mdlMostrarProveedores($campo, $valor);
+
+        return $respuesta;
+    }
+
+    /*=============================================
+      EDITAR
+    =============================================*/
+    static public function ctrEditarProveedor(){        
+
+            if(isset($_POST["editarProveedor"])){                            
+
+                            $datos = array( "idProveedor"         => $_POST["idProveedor"],
+                                            "nit"         => $_POST["editarNit"],
+                                            "nombre"      => $_POST["editarProveedor"],
+                                            "direccion"   => $_POST["editarDireccion"],
+                                            "telefono1"   => $_POST["editarTelefono1"],
+                                            "telefono2"   => $_POST["editarTelefono2"],
+                                            "idCiudad"    => $_POST["editarCiudad"]);
+
+                            $respuesta = ModeloProveedores::mdlEditarProveedor($datos);
+
+                            if($respuesta == "ok"){
+
+                                    echo'<script>
+                                    swal({
+                                            type: "success",
+                                            title: "EL proveedor ha sido editado correctamente",
+                                            showConfirmButton: true,
+                                            confirmButtonText: "Cerrar"
+                                            }).then(function(result){
+                                                    if (result.value) {
+                                                        window.location = "proveedores";
+                                                    }
+                                            })
+                                    </script>';
+                            } else {
+                                echo '<script>
+                                        swal({
+                                                type: "error",
+                                                title: "'.$respuesta.'",
+                                                showConfirmButton: true,
+                                                confirmButtonText: "Cerrar"
+
+                                        }).then(function(result){
+                                                if(result.value){
+                                                        window.location = "proveedores";
+                                                }
+                                        });
+                                </script>';                               
+                            }
+
+            }
+    }
+    
+    /*=============================================
+      BORRAR
+    =============================================*/
+    static public function ctrBorrarProveedor(){
+
+        if(isset($_GET["idProveedor"])){
+                
+                $datos = $_GET["idProveedor"];
+
+                $respuesta = ModeloProveedores::mdlBorrarProveedor($datos);
+
+                if($respuesta == "ok"){
+
+                        echo'<script>
+
+                        swal({
+                                  type: "success",
+                                  title: "El proveedor ha sido borrado correctamente",
+                                  showConfirmButton: true,
+                                  confirmButtonText: "Cerrar"
+                                  }).then(function(result){
+                                                        if (result.value) {
+
+                                                        window.location = "proveedores";
+
+                                                        }
+                                                })
+
+                        </script>';
+                }
+        }
+    }
+}            
+
