9330ba1a486c8c35bf8fd9f4bf60afb60660d9ed
Carlos Ubntu <carlos.arturo.hoyos@gmail.com>
2019-03-09 20:47:03 -0500

    Prueba NetBeans Ubuntu

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -3,5 +3,5 @@
 //info de la base de datos
 define('NOMBRE_SERVIDOR', 'localhost');
 define('NOMBRE_USUARIO', 'root');
-define('PASSWORD', '');
+define('PASSWORD', 'Anasofia2012');
 define('NOMBRE_BD', 'acot_desa');
